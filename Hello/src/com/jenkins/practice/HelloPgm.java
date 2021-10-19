92d81a9927644ea84b355e09b0ed5cd687ac49c0
Monika Attuluri <monika.chowdary16@gmail.com>
2021-10-19 20:48:34 +0530

    Commiting my Hellopgm to Sample repo

---
+++ Hello/src/com/jenkins/practice/HelloPgm.java
@@ -0,0 +1,11 @@
+package com.jenkins.practice;
+
+public class HelloPgm {
+
+	public static void main(String[] args) 
+	{
+		
+		System.out.println("Hey girl, All the best for your Jenkins Practice");
+	}
+
+}
